440e5f4a733c6876b182326e834f758a9939d25c
raushangpt <rk880488@gmail.com>
2023-03-10 05:20:30 +0530

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,5 +7,26 @@ module.exports = {
         },
         extend: {},
     },
-    plugins: [],
+    plugins: [require('daisyui')],
+
+    daisyui: {
+        styled: false, // true
+        utils: false, // true
+        logs: false, // true
+        themes: [
+            {
+                mytheme: {
+                    'primary': '#3a72ad',
+                    'secondary': '#8270c9',
+                    'accent': '#ed9e28',
+                    'neutral': '#1B2731',
+                    'base-100': '#39313F',
+                    'info': '#9ACFDF',
+                    'success': '#17A173',
+                    'warning': '#F5BD14',
+                    'error': '#E35F86',
+                },
+            },
+        ],
+    },
 };
